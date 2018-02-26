43324806d944d0667d3ba8268a71159be2a73284
Rob Lihou <rob@visiosound.co.uk>
2018-02-26 21:27:59 +0000

    Step 5 complete

--- __tests__/Pet.tests.js
+++ __tests__/Pet.tests.js
@@ -9,6 +9,19 @@ test ('Pet creates a new pet object with a name property', () => {
 test ('growUp adds 1 year to pets age', () => {
   const Fido = new Pet('Fido')
   Fido.growUp();
-  Fido.growUp();
-  expect(Fido.age).toEqual(2);
+  expect(Fido.age).toEqual(1);
 });
+
+test ('growUp increases hunger by 5', () => {
+  const Fido = new Pet('Fido')
+  Fido.growUp();
+  expect(Fido.hunger).toEqual(5);
+});
+
+test ('growUp decreases fitness by 3', () => {
+  const Fido = new Pet('Fido')
+  Fido.growUp();
+  expect(Fido.fitness).toEqual(7);
+});
+
+
