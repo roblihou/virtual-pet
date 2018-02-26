350c3ea9a4539688a850f13bd99a45127877baaa
Rob Lihou <rob@visiosound.co.uk>
2018-02-26 21:09:46 +0000

    added growUp function

--- __tests__/Pet.tests.js
+++ __tests__/Pet.tests.js
@@ -1,9 +1,13 @@
 const { Pet } = require('../src/Pet');
 
+
 test ('Pet creates a new pet object with a name property', () => {
-
   const Fido = new Pet('Fido')
-
   expect(Fido.name).toEqual('Fido');
+});
 
+test ('growUp adds 1 year to pets age', () => {
+  const Fido = new Pet('Fido')
+  Fido.growUp();
+  expect(fido.age).toEqual(1);
 });
--- src/Pet.js
+++ src/Pet.js
@@ -1,6 +1,15 @@
 function Pet (name) {
   this.name = name;
+  this.age = 0;
 };
+
+Pet.prototype = {
+  growUp: function () {
+    this.age++;
+  }
+}
+
+
 
 module.exports = {
   Pet
